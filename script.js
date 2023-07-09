10ae0b1d4dff283f6be108f186af5dd9b7c60934
Heitor <49591729+hectorclakrag@users.noreply.github.com>
2023-07-09 10:02:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
+// guardei numa variavel o input type date com a id date
 let userInput = document.getElementById("date");
+// aqui e uma tecnica para desabilitar datas futuras
 userInput.max = new Date().toISOString().split("T")[0];
+// uma variavel para mostrar as respostas
 let result = document.getElementById("result");
 
+// onde a magica acontece!
 function calculateAge() {
+    // uma variavel para guardar os valores da input guardadas acima em outra variavel userInput
     let birthDate = new Date(userInput.value);
 
+    // variaveis para guardar o dia, mes e ano selecionados na input
     let d1 = birthDate.getDate();
     let m1 = birthDate.getMonth() + 1;
     let y1 = birthDate.getFullYear();
 
+    // uma variavel para guardarmos a data atual 
     let today = new Date();
 
+    // novamente guardando os valores do dia, mes e ano atuais
     let d2 = today.getDate();
     let m2 = today.getMonth() + 1;
     let y2 = today.getFullYear();
 
+    // variaveis para guardar os valores do resultado
     let d3, m3, y3;
 
+    // calculo do ano
     y3 = y2 - y1;
 
+    // calculo do mes
     if (m2 >= m1) {
         m3 = m2 - m1;
     } else {
